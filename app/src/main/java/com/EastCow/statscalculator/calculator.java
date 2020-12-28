5a6546395a5f7605f74eb35b5a0d2e293408086c
cheng <n545318582@outlook.com>
2020-12-28 16:01:29 -0400

    initial commit

---
+++ app/src/main/java/com/EastCow/statscalculator/calculator.java
@@ -0,0 +1,37 @@
+package com.EastCow.statscalculator;
+
+import java.util.Scanner;
+
+public class calculator {
+    private static int n;
+    private static int nFact;
+    private static int r;
+    private static int rFact;
+    private static int nrFact;
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("give the data, n for objects, r for samples.");
+        System.out.println("n");
+        n = in.nextInt();
+        nFact = getFactorial(n);
+        System.out.println("r");
+        r = in.nextInt();
+        rFact = getFactorial(r);
+        int nr = n - r;
+        nrFact = getFactorial(nr);
+
+        System.out.println("Combination answer is: " + nFact / (rFact * nrFact));
+        System.out.println("Permutation answer is: " + nFact / nrFact);
+    }
+
+    public static int getFactorial(int data) {
+        int i;
+        int fact = 1;
+        int num = data;
+        for (i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+}
